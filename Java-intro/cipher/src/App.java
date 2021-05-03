51ec741fc086374ae1b33fc5774fc5d1100c5bb7
rudger <klugr2007@gmail.com>
2021-05-03 17:57:20 -0600

    finished encoder/decoder

--- Java-intro/cipher/src/App.java
+++ Java-intro/cipher/src/App.java
@@ -1,7 +1,20 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int n= Integer.parseInt(args[1]);
-        System.out.println(args[0]);
-        System.out.println(n);
+        String plaintext = "Wziljw%nx%f%uwjxts%|mt%htijx%ns%of{f%xhwnuy";
+        int n= 5;
+        String ciphertext = App.decrypt (plaintext, n);
+        System.out.println (ciphertext);
     }
-}
+    public static String encrypt(String plaintext,int n){
+        StringBuilder s =new StringBuilder();
+        for (char c : plaintext.toCharArray()){
+            s.append((char)(c+n));
+        }
+        return s.toString();
+    }
+    public static String decrypt (String ciphertext, int n) {
+     return  App.encrypt (ciphertext, -n);
+        
+    }
+        
+} 
